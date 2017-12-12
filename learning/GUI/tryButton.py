200914a6d0a7a443bcfae39544499e8c8cd86c29
Lunchesque <lunchesque1993@gmail.com>
2017-12-12 15:21:47 +0300

    ASD

--- learning/GUI/tryButton.py
+++ learning/GUI/tryButton.py
@@ -1,8 +1,4 @@
 from tkinter import *
 
-root = Tk()
 
-but = Button(root, text = "This is a btn", width = 30, height = 5, bg = "white", fg = "blue")
-lab = Label(root, text="Это метка! \n Из двух строк.", font="Arial 18")
-but.pack()
 root.mainloop()
