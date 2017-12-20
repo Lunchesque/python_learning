36ca434648a7e738d0a857d6c2ab2ef0bc1b6024
Lunchesque <lunchesque1993@gmail.com>
2017-12-20 11:56:30 +0300

    plan

--- learning/plan/plan.py
+++ learning/plan/plan.py
@@ -4,8 +4,10 @@ RAM 4GB         ->      80r
 
 Mot.Board       ->      90r
 
-CPU fx-6300     ->      150r
+CPU fx-8300     ->      190r
 
-General:    jan - 260r
-            feb - 150r
-            mar - 200r
+ssd 240GB       ->      200r
+
+General:    jan - 260r (ssd 60GB, RAM 4GB, MotherBoard)
+            feb - 190r (CPU fx-8300)
+            mar - 200r (ssd 240GB)
