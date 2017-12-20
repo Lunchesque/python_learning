938a4caca9aca005bf97acb1eefc6226eaee7442
Lunchesque <lunchesque1993@gmail.com>
2017-12-20 11:43:24 +0300

    plan

---
+++ learning/plan/plan.py
@@ -0,0 +1,11 @@
+ssd 60GB        ->      90r
+
+RAM 4GB         ->      80r
+
+Mot.Board       ->      90r
+
+CPU fx-6300     ->      150r
+
+General:    jan - 260r
+            feb - 150r
+            mar - 200r
