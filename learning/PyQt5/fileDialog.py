0a8bab23332c01f9bb339520bee4dad583f3d4e4
Lunchesque <lunchesque1993@gmail.com>
2017-12-20 10:19:53 +0300

    filedialog

--- learning/PyQt5/fileDialog.py
+++ learning/PyQt5/fileDialog.py
@@ -2,7 +2,8 @@
 #-*- coding: utf-8 -*-
 
 import sys
-from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import (QMainWindow, QTextEdit, QAction,
+    QFileDialog, QApplication)
 from PyQt5.QtGui import QIcon
 
 class FileDialog(QMainWindow):
@@ -16,15 +17,19 @@ class FileDialog(QMainWindow):
 
         self.textEdit = QTextEdit()
         self.setCentralWidget(self.textEdit)
-        self.statusBar()
 
         openFile = QAction(QIcon('open.png'), '&Open', self)
         openFile.setShortcut('Ctrl+O')
         openFile.setStatusTip('Open new file')
         openFile.triggered.connect(self.showDialog)
 
+        self.statusBar()
+
+        self.toolbar = self.addToolBar('&Open')
+        self.toolbar.addAction(openFile)
+
         menubar = self.menuBar()
-        fileMenu = menubar.addMenu('&File')
+        fileMenu = menubar.addMenu('&file')
         fileMenu.addAction(openFile)
 
         self.setGeometry(300, 300, 300, 300)
