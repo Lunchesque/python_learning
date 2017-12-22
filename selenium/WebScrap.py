657fa08dca1ed6e66461b9705d679c4bb545da7a
Lunchesque <lunchesque1993@gmail.com>
2017-12-22 14:13:28 +0300

    selenium

--- selenium/WebScrap.py
+++ selenium/WebScrap.py
@@ -1,34 +1,11 @@
-import time
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
-from selenium.common.exceptions import ElementNotVisibleException
+from selenium.webdriver.common.keys import Keys
 
-def init_driver():
-    driver = webdriver.Firefox()
-    driver.wait = WebDriverWait(driver, 5)
-    return driver
-
-def lookup(driver, query):
-    driver.get('http://www.google.ru')
-    try:
-        box = driver.wait.until(EC.presence_of_element_located(
-            (By.NAME, 'q')))
-        button = driver.wait.until(EC.element_to_be_clickable(
-            (By.NAME, 'btnK')))
-        box.send_keys(query)
-        try:
-            button.click()
-        except ElementNotVisibleException:
-            button = driver.wait.until(EC.visibility_of_element_located(
-                (By.NAME, "btnG")))
-    except TimeoutException:
-        print("Box or button not found in google.com")
-
-if __name__ == '__main__':
-    driver = init_driver()
-    lookup(driver, "Selenium")
-    time.sleep(5)
-    driver.quit()
+driver = webdriver.Firefox()
+driver.get("http://www.python.org")
+assert "Python" in driver.title
+elem = driver.find_element_by_name("q")
+elem.send_keys("pycon")
+elem.send_keys(Keys.RETURN)
+assert "No results found" not in driver.page_source
+driver.close()
---
+++ selenium/sech.py
@@ -0,0 +1,20 @@
+import time
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+class TestPage(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+
+    def test_sech(self):
+        driver = self.driver
+        driver.get("http://sech.esy.es")
+
+    def tearDown(self):
+        time.sleep(5)
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
